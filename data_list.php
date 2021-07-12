430f58648876bbb5e04102dd437e320874d4856c
himani1295 <himanishinedezign@gmail.com>
2021-07-12 22:29:55 +0530

    View employee data

--- data_list.php
+++ data_list.php
@@ -1,1 +1,28 @@
-
+<?php
+//database configure
+include ('/connection/connection.php');
+?>
+<html>
+<head><title>Data Listing</title></head>
+<body>
+	<table border="1">
+		<tr>
+		<th>Name</th>
+		<th>Email</th>
+		<th>Designation</th>
+		<th>Image</th>
+	</tr>
+	<?php 
+	$select="SELECT * FROM `employee`";
+	$result=mysqli_query($conn,$select);
+ while($row=mysqli_fetch_assoc($result)){?>
+ 	<td><?php echo $row['email']?></td>
+ 	<td><?php echo $row['designation']?></td>
+ 	<td><?php echo $row['email']?></td>
+ <?php }
+	?>
+		<tr>
+			<td></td>
+		</tr>
+	</table>
+	</body>
